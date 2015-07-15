03b90a0f74238792cf72d38339ad2724f3edffca
Frieder Steinmetz <frieder.steinmetz@tuhh.de>
2015-07-15 18:09:44 +0200

    Update filecp.sh

--- filecp.sh
+++ filecp.sh
@@ -9,12 +9,6 @@ umount $MNTPOINT
 mount -oro,loop,offset=$((2048 * 512)) -t vfat $IMG $MNTPOINT
 find $MNTPOINT -type f -exec du -b {} \; > tree2
 
-# awk searches backwards for the first occurance of a digit
-# and prints the field to right of said digit
-# example:
-# 							      >	1357	/mnt/pendrive/somefile
-#                       |^|________searches for this nr and prints the path to it's right
-#
 diff -y --suppress-common-lines -W 20000 tree1 tree2 | rev | cut -f 1 | rev | while read FILE
 do
   if echo $FILE | grep -q "<$"; then
@@ -23,4 +17,3 @@ do
     cp -v "${FILE[@]}" ${UPLOADDIR}
   fi
 done
-
